eda00102aa2d3baadfae9b010cbee560c193dc1b
garaos <g.araos@gmail.com>
2023-01-23 21:17:29 -0300

    producto por ID

--- backend/controllers/products.controller.js
+++ backend/controllers/products.controller.js
@@ -1,7 +1,7 @@
 
 const Product = require ('../models/product.model')
 
-// nuevo producto
+// nuevo producto ->http://localhost:4000/api/v1/products/new
 exports.newProduct = async (req, res)=>{
     const product = await Product.create(req.body);
     res.status(201).json({
@@ -10,13 +10,29 @@ exports.newProduct = async (req, res)=>{
     })
 }
 
-// todos los productos
+// todos los productos -> http://localhost:4000/api/v1/products
 exports.getProducts = async (req, res) =>{
     const product = await Product.find();
 
     res.status(200).json({
         success: true,
         count: product.length,
+        product
+    })
+}
+
+// producto por ID -> http://localhost:4000/api/v1/products/:id
+exports.getProduct = async (req, res) =>{
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Not found'
+        })
+    }
+    res.status(200).json({
+        success: true,
         product
     })
 }
