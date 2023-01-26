cbe571fd786d795401fd944efbfdb243115a3296
garaos <g.araos@gmail.com>
2023-01-26 19:25:31 -0300

    CRUD

--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -2,14 +2,16 @@
 const express = require('express');
 const productsRouter = express.Router();
 
-const { getProducts, newProduct, getProduct } = require ('../controllers/products.controller')
+const { getProducts, newProduct, getProduct,updateProduct, deleteProduct } = require ('../controllers/products.controller')
 
 
 const baseURI = '/products'
 
 productsRouter.get(baseURI, getProducts)
-productsRouter.post(`${baseURI}/new`, newProduct)
+productsRouter.post(`/admin${baseURI}/new`, newProduct)
 productsRouter.get(`${baseURI}/:id`, getProduct)
+productsRouter.put(`/admin${baseURI}/:id`, updateProduct)
+productsRouter.delete(`/admin${baseURI}/:id`, deleteProduct)
 
 //router.route('/products').get(getProducts);
 // router.route('/products/new').post(newProduct);
